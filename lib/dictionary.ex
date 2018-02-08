b891c0ab65a3a8cd996525128f0435cf1afb5204
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-02-07 23:02:43 -0800

    add

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,5 +1,12 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts("Hello world!!")
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+
+  def word_list() do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
 end
