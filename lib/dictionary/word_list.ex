8ac4ab4692936e0b6e90311fdb5f4b6859f8baf5
Steven Magelowitz <steven.magelowitz@gmail.com>
2018-02-17 12:49:03 +0800

    run everything in processes

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,10 +1,19 @@
 defmodule Dictionary.WordList do
-  def random_word(word_list) do
-    word_list
-    |> Enum.random()
+  @me __MODULE__
+
+  def start_link() do
+    Agent.start_link(&word_list/0, name: @me)
   end
 
-  def word_list() do
+  def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+
+    Agent.get(@me, &Enum.random/1)
+  end
+
+  defp word_list() do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
     |> File.read!()
